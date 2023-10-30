fc28d3e125b0ff1b3402edd402164d68fb2177a5
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-30 16:13:40 -0400

    Working with responses: HTTP Codes, Exceptions & JSON Data

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+# print(response)
+
+# Different Responses
+# 1XX: Hold on
+# 2XX: Here You Go
+# 3XX: Go away
+# 4XX: You Screwed up
+# 5XX: Server Screwed up
+
+response.raise_for_status()
+data = response.json()
+
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+
+print(iss_position)
